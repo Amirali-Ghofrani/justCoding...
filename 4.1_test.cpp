5c4eb019ce4535516f43142ced9fa14f28263833
Amirali_Ghofrani <ghofrani.cw@gmail.com>
2024-12-31 03:36:38 +0330

    Declaring the function in a seperate .h file and including the library

--- 4.1_test.cpp
+++ 4.1_test.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 void test()  // Just a simple test function to declare in 4_headerFiles
 {
-    std::cout<<"I am a test function for links!";
+    std::cout<<"I am a test function for links!"<<std::endl;
+}
+
+void test2()
+{
+   std::cout<<"The second test function is also working just fine!"<<std::endl;
+
 }
--- 4_headerFiles.cpp
+++ 4_headerFiles.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
-int test();   // Declaring the function defined in 4.1_test file
+#include "4.2_library.h" // includes declaration for the second test function: test2()
+
+int test();              // Declaring the function defined in 4.1_test file
+
 int main()
 {
    test();    // calling the declared function
-   std::cout<<"Congrats! Your function is working!";
+   std::cout<<"Congrats! Your function is working!"<<std::endl;
+   test2();   // calling the secong test function
    return 0;
 }
