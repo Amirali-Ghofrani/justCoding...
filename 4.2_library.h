febf3f1ad4e89bb2cdc48cba63cc62f019ee8484
Amirali_Ghofrani <ghofrani.cw@gmail.com>
2024-12-31 12:31:33 +0330

    header files -update

--- 4.2_library.h
+++ 4.2_library.h
@@ -1,1 +1,14 @@
+#pragma once // prevents including functions twice and a subsequent duplicate error
 void test2();
+
+//another old-fashioned header gaurd which 
+//can be used instead of pragma once: 
+
+// #ifdef
+// #define
+//. 
+//.
+//.
+
+// #endif
+
--- 4_headerFiles.cpp
+++ 4_headerFiles.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include "4.2_library.h" // includes declaration for the second test function: test2()
+                        // <> are used for include directories and "" are used when the
+                        // library in in the current directory
+// we can also use "" instead of <>, but not vice versa!
 
 int test();              // Declaring the function defined in 4.1_test file
 
