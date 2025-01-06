bbca9ec0b223f78472673c32f0a7d00a4e2cc1a0
Amirali_Ghofrani <ghofrani.cw@gmail.com>
2025-01-06 16:21:13 +0330

    Const in Classes

--- 025_const.cpp
+++ 025_const.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 #include <string>
 
+class Entity
+{
+ private:
+    int m_X, m_Y;
+    int* m_Z, m_A, *m_B; //m_Z and m_B are pointers but m_A is just an integer!
+    mutable int var;
+
+public:
+    int GetX() const  //this example works only for classes and it means we cannot modify
+    {                //the contents of this class and our method is read-only
+        //m_X = 2; //causes ERRORs!
+        var = 2;   // we can change mutable objects even in const functions
+        return m_X;
+    }
+
+    const int* const GetX_2() //if z was a pointer and needed to be const
+        {                   // this means the function returns a pointer which cannot
+            return m_Z;    //be modified, the value of the pointer cannot be modified either
+        }                 //and the function in a red-only func and wont modify the actual Entity class 
+
+};
+
 int main()
 {
     const int MAX_AGE = 90; //some kind of declaration that we dont want our 
@@ -19,5 +41,6 @@ int main()
 
     const int* const c = new int;
     //when we write const twice, we neither can change the value nor what it points to
+
 
 }
