0b9c2ba5eba17fe210e31b9c810abadf3c0436e0
Amirali_Ghofrani <ghofrani.cw@gmail.com>
2025-02-18 22:48:29 +0330

    Comments Update

--- learncpp_tutorials/00507_chapter5Quiz.cpp
+++ learncpp_tutorials/00507_chapter5Quiz.cpp
@@ -1,7 +1,14 @@
+/*
+Write a program that asks the user to enter their full name and their age. 
+As output, tell the user the sum of their age and the number of characters in 
+their name (use the std::string::length() member function to get the length of the string).
+For simplicity, count any spaces in the name as a character.
+*/
+
 #include <iostream>
 #include <string>
 
-std::string getName()
+std::string getName()   //getting full name of the user
 {
     std::cout << "Please Enter Your Full Name: ";
     std::string name{};
@@ -10,14 +17,14 @@ std::string getName()
     return name;
 }
 
-int nameLength(const std::string name)
+int nameLength(const std::string name) //determining the length of the user's name
 {
     int len{};
     len = static_cast<int>(name.length());
     return len;
 }
 
-int getAge()
+int getAge()            // getting age of the user
 {
     std::cout << "Please Enter Your Age: ";
     int age{};
--- learncpp_tutorials/00510_chapter5Quiz4.cpp
+++ learncpp_tutorials/00510_chapter5Quiz4.cpp
@@ -1,8 +1,13 @@
+/*
+Write a program that asks for the name and age of two people, then prints which person is older.
+Here is the sample output from one run of the program:
+*/
+
 #include <iostream>
 #include <string>
 #include <string_view>
 
-std::string getName(int count)
+std::string getName(int count) // getting name of the user
 {
     std::cout << "Please Enter the name of person #" << count <<": ";
     std::string name;
@@ -11,7 +16,7 @@ std::string getName(int count)
     return name;
 }
 
-int getAge(int count)
+int getAge(int count)       // getting age of the user
 {
     std::cout << "Please enter the age of person #" << count <<": ";
     int age{};
@@ -19,6 +24,8 @@ int getAge(int count)
 
     return age;
 }
+
+// comparing ages
 
 void print (std::string_view name1, int age1, std::string_view name2, int age2)
 {
