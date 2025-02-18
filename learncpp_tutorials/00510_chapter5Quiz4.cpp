f4b1d6e14b954112955ff9d0dad009b5f7e0ab85
Amirali_Ghofrani <ghofrani.cw@gmail.com>
2025-02-18 22:28:43 +0330

    Chapter 5 Quiz 4, With Using string_view and const

--- learncpp_tutorials/00510_chapter5Quiz4.cpp
+++ learncpp_tutorials/00510_chapter5Quiz4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <string_view>
 
 std::string getName(int count)
 {
@@ -19,19 +20,8 @@ int getAge(int count)
     return age;
 }
 
-
-int main()
+void print (std::string_view name1, int age1, std::string_view name2, int age2)
 {
-    int count{1};
-
-    std::string name1 {getName(count)};
-    int age1 {getAge(count)};
-
-    ++count;
-
-    std::string name2 {getName(count)};
-    int age2 {getAge(count)};
-
     if (age1 > age2)
         std::cout << name1 <<" (age " << age1 <<") is older than " << name2
         << " (age " << age2 <<")\n"; 
@@ -39,5 +29,22 @@ int main()
     else if (age1 < age2)
     std::cout << name2 <<" (age " << age2 <<") is older than " << name1
     << " (age " << age1 <<")\n"; 
+
+}
+
+
+int main()
+{
+    int count{1};
+
+    const std::string name1 {getName(count)};
+    const int age1 {getAge(count)};
+
+    ++count;
+
+    const std::string name2 {getName(count)};
+    const int age2 {getAge(count)};
+
+    print(name1, age1, name2, age2);
 
 }
